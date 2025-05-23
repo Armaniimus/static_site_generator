7ca6cdfabca7a1a292f1203ffe447e414b2d68e5
Armaniimus <Tommy_chester@hotmail.com>
2025-05-23 09:12:32 +0200

    added textnodes with tests

---
+++ src/test_textnode.py
@@ -0,0 +1,31 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+	def test_eq(self):
+		node = TextNode("This is a text node", TextType.BOLD)
+		node2 = TextNode("This is a text node", TextType.BOLD)
+		
+		self.assertEqual(node, node2)
+
+	def test_unequal(self):
+		node = TextNode("This is a text node", TextType.BOLD)
+		node2 = TextNode("This is", TextType.BOLD)
+		node3 = TextNode("This is a text node", TextType.NORMAL)
+
+		self.assertNotEqual(node, node2)
+		self.assertNotEqual(node, node3)
+
+	def test_is_url_none(self):
+		node = TextNode("This is a text node", TextType.BOLD)
+
+		self.assertEqual(node.url, None)
+	
+	def test_is_url_set(self):
+		node = TextNode("This is a text node", TextType.IMAGE, "./a.jpg")
+
+		self.assertEqual(node.url, "./a.jpg")
+if __name__ == "__main__":
+		unittest.main()
