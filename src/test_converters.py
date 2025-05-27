343512a4485dc1a2c5b944bc439ba441dc5f3b7c
Armaniimus@laptop <tommy_chester@hotmail.com>
2025-05-27 18:17:32 +0200

    added tests for the functions that splitout images and links

--- src/test_converters.py
+++ src/test_converters.py
@@ -179,6 +179,19 @@ class TestSplitNodesDelimiter(unittest.TestCase):
 
 	def test_split_images(self):
 		node = TextNode(
+			"This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)", TextType.TEXT,
+		)
+		new_nodes = split_nodes_image([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.TEXT),
+				TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png")
+			],
+			new_nodes,
+		)
+
+	def test_split_images_x2(self):
+		node = TextNode(
 			"This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
 			TextType.TEXT,
 		)
@@ -191,6 +204,36 @@ class TestSplitNodesDelimiter(unittest.TestCase):
 				TextNode(
 					"second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
 				),
+			],
+			new_nodes,
+		)
+
+	def test_split_link(self):
+		node = TextNode(
+			"This is text with an [link](https://i.imgur.com)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.TEXT),
+				TextNode("link", TextType.IMAGE, "https://i.imgur.com"),
+			],
+			new_nodes,
+		)
+
+	def test_split_link_x2(self):
+		node = TextNode(
+			"This is text with an [link](https://i.imgur.com) and another [second link](https://i.imgur.com)",
+			TextType.TEXT,
+		)
+		new_nodes = split_nodes_link([node])
+		self.assertListEqual(
+			[
+				TextNode("This is text with an ", TextType.TEXT),
+				TextNode("link", TextType.IMAGE, "https://i.imgur.com"),
+				TextNode(" and another ", TextType.TEXT),
+				TextNode("second link", TextType.IMAGE, "https://i.imgur.com"),
 			],
 			new_nodes,
 		)
