2f3c3244af67fd22df35a3e227463bc76619c82c
Armaniimus@laptop <tommy_chester@hotmail.com>
2025-05-31 13:31:56 +0200

    added markdown to blocks function with unit tests

--- src/blockconverter.py
+++ src/blockconverter.py
@@ -1,3 +1,14 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+	PARAGRAPH = "paragraph"
+	HEADING = "heading"
+	CODE = "code"
+	QUOTE = "quote"
+	UNORDERED_LIST = "unordered_list"
+	ORDERED_LIST = "ordered_list"
+
 def markdown_to_blocks(markdown):
 	blocks = markdown.split("\n\n")
 
@@ -8,3 +19,37 @@ def markdown_to_blocks(markdown):
 	filtered_blocks = list( filter(func, blocks) )
 	
 	return filtered_blocks
+
+def block_to_block_type(block):
+	is_heading = re.match(r"^[#]{1,6} \S", block) != None
+	is_code = re.fullmatch(r"^```[\s\S]+```$", block) != None
+	is_quote_regex = r"^>[\s\S]+"
+	is_unordered_list_regex = r"^- [\s\S]+"
+	is_ordered_list_regex = r"^[0-9]+\. [\s\S]+"
+
+	if is_heading:
+		return BlockType.HEADING
+	
+	elif is_code:
+		return BlockType.CODE
+	else:
+		def checker(regex):
+			lines = block.split("\n")
+			for l in lines:
+				if re.fullmatch(regex, l) == None:
+					return False
+				
+			return True
+		
+		is_quote = checker(is_quote_regex)
+		is_unordered_list = checker(is_unordered_list_regex)
+		is_ordered_list = checker(is_ordered_list_regex)
+
+		if is_quote:
+			return BlockType.QUOTE
+		elif is_unordered_list:
+			return BlockType.UNORDERED_LIST
+		elif is_ordered_list:
+			return BlockType.ORDERED_LIST
+	
+	return BlockType.PARAGRAPH
--- src/test_blockconverter.py
+++ src/test_blockconverter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from blockconverter import markdown_to_blocks
+from blockconverter import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestTextNodeToHtml(unittest.TestCase):
@@ -61,6 +61,109 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+	
+	def test_block_to_block_type_heading_first(self):
+		text = "# heading1"
+
+		actual = block_to_block_type(text)
+
+		self.assertEqual(BlockType.HEADING, actual)
+
+	def test_block_to_block_type_heading_last(self):
+		text = "###### heading6"
+
+		actual = block_to_block_type(text)
+
+		self.assertEqual(BlockType.HEADING, actual)
+
+	def test_block_to_block_type_heading_all_cases(self):
+		headings = [
+			"# heading1",
+    		"## heading2",
+			"### heading3",
+			"#### heading4",
+			"##### heading5",
+			"###### heading6"
+		]
+
+		for h in headings:
+			actual = block_to_block_type(h)
+			message = f"\ntest failed: actual is not equal to BlockType.HEADING\nactual == {actual}\ntestedString == {h}"
+			self.assertEqual(BlockType.HEADING, actual, message)
+	
+	def test_block_to_block_type_heading_false_cases(self):
+		headings = [
+			"heading0",
+			"#heading1",
+    		"##heading2",
+			"###heading3",
+			"####heading4",
+			"#####heading5",
+			"######heading6",
+			"####### heading7"
+		]
+
+		for h in headings:
+			actual = block_to_block_type(h)
+			message = f"\ntest failed: actual is not equal to BlockType.PARAGRAPH\nactual == {actual}\ntestedString == {h}"
+			self.assertEqual(BlockType.PARAGRAPH, actual, message)
+
+	def test_block_to_block_type_code(self):
+		string = "``` ```"
+
+		actual = block_to_block_type(string)
+			
+		self.assertEqual(BlockType.CODE, actual)
+
+	def test_block_to_block_type_quote(self):
+		string = """>1
+> 2
+> 3"""
+
+		actual = block_to_block_type(string)
+			
+		self.assertEqual(BlockType.QUOTE, actual)
+
+	def test_block_to_block_type_unordered_list(self):
+		string = """- a
+- b
+- c"""
+
+		actual = block_to_block_type(string)
+			
+		self.assertEqual(BlockType.UNORDERED_LIST, actual)
+
+	def test_block_to_block_type_ordered_list(self):
+		string = """1. a
+2. b
+3. c"""
+
+		actual = block_to_block_type(string)
+			
+		self.assertEqual(BlockType.ORDERED_LIST, actual)
+
+	def test_block_to_block_type_paragraph(self):
+		paragraphs = [
+			">",
+			"-a",
+			"-",
+			"1.a",
+			"1 a",
+			"1.",
+			"` ```",
+			"`` ```",
+			"``` ``",
+			"``` `",
+			"`` ``",
+			"` ``",
+			"`` `",
+			"```"
+		]
+
+		for p in paragraphs:
+			actual = block_to_block_type(p)
+			message = f"\ntest failed: actual is not equal to BlockType.PARAGRAPH\nactual == {actual}\ntestedString == {p}"
+			self.assertEqual(BlockType.PARAGRAPH, actual, message)
 	
 if __name__ == "__main__":
 	unittest.main()
