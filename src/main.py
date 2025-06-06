5ca30dc93981aa9c755807e89571af43e1309772
Armaniimus@laptop <tommy_chester@hotmail.com>
2025-06-06 15:34:19 +0200

    added static file generation into public directory

--- src/main.py
+++ src/main.py
@@ -1,7 +1,54 @@
-from textnode import TextNode, TextType
-
+import os
+import shutil
+from main_utils import extract_title
 
 def main():
-	a = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-	print(a)
+	__clear_public()
+	__add_static_files()
+
+	# markdown = ""
+	# title = __extract_title(markdown)	
+
+def __add_static_files():
+	path = "./public"
+	if not os.path.exists(path):
+		os.mkdir("./public")
+		print("log=> created ./public directory")
+	
+	if not os.path.exists("./static"):
+		os.mkdir("./static")
+		print("log=> created ./static directory")
+
+	print("log=> start __copy_dir")
+	__copy_dir("./static", "./public")
+		
+def __copy_dir(source, destination):
+	items = os.listdir(source)
+
+	for i in items:
+		src = f"{source}/{i}"
+		dest = f"{destination}/{i}"
+		if os.path.isfile(src):
+			shutil.copy(src, dest)
+			print(f"\t\tcopied file: {src}")
+		else:
+			os.mkdir(dest)
+			print(f"\tcopied folder: {src}")
+			__copy_dir(src, dest)
+
+
+def __clear_public():
+	path = "./public"
+	if os.path.exists(path):
+		print("log=> ./public exists\nlog=> removing ./public")
+		
+		message = f"log=> shutil.rmtree({path}): succeeded\nlog=> ./public removed"
+		def onerror(): nonlocal message; message = f"shutil.rmtree({path}): failed"
+		
+		shutil.rmtree(path, onerror=onerror)
+		print(message)
+			
+	else:
+		print("log=> public doesn't exist on path:" + path)
+		print(f"log=> localdirs: {os.listdir('./')}")
 main()
