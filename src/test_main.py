353233815269235cec8ce42a4706126f7127ba08
Armaniimus <Tommy_chester@hotmail.com>
2025-06-09 13:35:06 +0200

    added test for the fet_destination_file function

--- src/test_main.py
+++ src/test_main.py
@@ -1,6 +1,6 @@
 import unittest
 from main_utils import extract_title, get_source_files, get_destination_file
-from main import main
+
 class TestMain(unittest.TestCase):
 	def test_extract_title(self):
 		input = "# Hello"
@@ -20,12 +20,27 @@ class TestMain(unittest.TestCase):
 	# 	# print(i)
 
 	def test_get_destination_file(self):
-		destination_file  = get_destination_file("./content/contact", "./content", "./public/")
-		print(destination_file)
+		expected = "./public/contact.html"
+		
+		actual  = get_destination_file("./content/contact", "./content", "./public/")
 
-	def test_main(self):
-		main()
-		print("ran main")
+		self.assertEqual(actual, expected)
+
+	def test_get_destination_file_bulk(self):
+		data = (
+			(("./content/contact/index.md", "./content", "./public/"), "./public/contact.html"),
+			(("./content/blog/glorfindel/index.md", "./content", "./public/"), "./public/blog/glorfindel.html"),
+			(("./content/index.md", "./content", "./public/"), "./public/index.html"),
+		)
+		
+		for d in data:
+			inputs = d[0]
+			expected = d[1]
+
+			actual  = get_destination_file(inputs[0], inputs[1], inputs[2])
+			
+			message = f"\nFailed: \n\tinput={inputs} \n\tactual={actual} \n\texpected={expected}"
+			self.assertEqual(actual, expected, message)
 
 if __name__ == "__main__":
 	unittest.main()
